4ff489df5072d8da1ccae9b1dd48c87061df3cc7
wzy-55555 <2214978386@qq.com>
2022-07-23 12:41:52 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -8,8 +8,7 @@ import torch.nn as nn
 from PIL import ImageDraw, ImageFont
 
 from nets.yolo import YoloBody
-from utils.utils import (cvtColor, get_classes, preprocess_input, resize_image,
-                         show_config)
+from utils.utils import cvtColor, get_classes, preprocess_input, resize_image
 from utils.utils_bbox import decode_outputs, non_max_suppression
 
 '''
@@ -24,7 +23,7 @@ class YOLO(object):
         #   训练好后logs文件夹下存在多个权值文件，选择验证集损失较低的即可。
         #   验证集损失较低不代表mAP较高，仅代表该权值在验证集上泛化性能较好。
         #   如果出现shape不匹配，同时要注意训练时的model_path和classes_path参数的修改
-        #--------------------------------------------------------------------------#
+        #---------------------------------model_data/yolox_s.pth-----------------------------------------#
         "model_path"        : 'model_data/yolox_s.pth',
         "classes_path"      : 'model_data/coco_classes.txt',
         #---------------------------------------------------------------------#
@@ -38,11 +37,11 @@ class YOLO(object):
         #---------------------------------------------------------------------#
         #   只有得分大于置信度的预测框会被保留下来
         #---------------------------------------------------------------------#
-        "confidence"        : 0.5,
+        "confidence"        :  0.5,
         #---------------------------------------------------------------------#
         #   非极大抑制所用到的nms_iou大小
         #---------------------------------------------------------------------#
-        "nms_iou"           : 0.3,
+        "nms_iou"           :  0.3,
         #---------------------------------------------------------------------#
         #   该变量用于控制是否使用letterbox_image对输入图像进行不失真的resize，
         #   在多次测试后，发现关闭letterbox_image直接resize的效果更好
@@ -69,7 +68,6 @@ class YOLO(object):
         self.__dict__.update(self._defaults)
         for name, value in kwargs.items():
             setattr(self, name, value)
-            self._defaults[name] = value 
             
         #---------------------------------------------------#
         #   获得种类和先验框的数量
@@ -83,27 +81,26 @@ class YOLO(object):
         self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
         self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
         self.generate()
-        
-        show_config(**self._defaults)
 
     #---------------------------------------------------#
     #   生成模型
     #---------------------------------------------------#
-    def generate(self, onnx=False):
+    def generate(self):
         self.net    = YoloBody(self.num_classes, self.phi)
         device      = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
         self.net.load_state_dict(torch.load(self.model_path, map_location=device))
         self.net    = self.net.eval()
         print('{} model, and classes loaded.'.format(self.model_path))
-        if not onnx:
-            if self.cuda:
-                self.net = nn.DataParallel(self.net)
-                self.net = self.net.cuda()
+
+        if self.cuda:
+            self.net = nn.DataParallel(self.net)
+            self.net = self.net.cuda()
 
     #---------------------------------------------------#
     #   检测图片
     #---------------------------------------------------#
-    def detect_image(self, image, crop = False, count = False):
+    def detect_image(self, image, crop = False):
         #---------------------------------------------------#
         #   获得输入图片的高和宽
         #---------------------------------------------------#
@@ -149,18 +146,7 @@ class YOLO(object):
         #---------------------------------------------------------#
         font        = ImageFont.truetype(font='model_data/simhei.ttf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness   = int(max((image.size[0] + image.size[1]) // np.mean(self.input_shape), 1))
-        #---------------------------------------------------------#
-        #   计数
-        #---------------------------------------------------------#
-        if count:
-            print("top_label:", top_label)
-            classes_nums    = np.zeros([self.num_classes])
-            for i in range(self.num_classes):
-                num = np.sum(top_label == i)
-                if num > 0:
-                    print(self.class_names[i], " : ", num)
-                classes_nums[i] = num
-            print("classes_nums:", classes_nums)
+        
         #---------------------------------------------------------#
         #   是否进行目标的裁剪
         #---------------------------------------------------------#
@@ -182,37 +168,107 @@ class YOLO(object):
         #---------------------------------------------------------#
         #   图像绘制
         #---------------------------------------------------------#
+
+        # wzy定义保存框信息的列表
+        list_all = []
+
         for i, c in list(enumerate(top_label)):
-            predicted_class = self.class_names[int(c)]
-            box             = top_boxes[i]
-            score           = top_conf[i]
+            if self.class_names[int(c)] == 'car' or self.class_names[int(c)] == 'person':
+                predicted_class = self.class_names[int(c)]
+                print(self.class_names[int(c)])
+                box             = top_boxes[i]
+                score           = top_conf[i]
+                top, left, bottom, right = box
 
-            top, left, bottom, right = box
+                top = max(0, np.floor(top).astype('int32'))
+                left = max(0, np.floor(left).astype('int32'))
+                bottom = min(image.size[1], np.floor(bottom).astype('int32'))
+                right = min(image.size[0], np.floor(right).astype('int32'))
 
-            top     = max(0, np.floor(top).astype('int32'))
-            left    = max(0, np.floor(left).astype('int32'))
-            bottom  = min(image.size[1], np.floor(bottom).astype('int32'))
-            right   = min(image.size[0], np.floor(right).astype('int32'))
+                # wzy将信息组合塞入list中 [predicted_class, top, left, bottom, right]
+                list_single = [predicted_class, top, left, bottom, right]
+                list_all.append(list_single)
 
-            label = '{} {:.2f}'.format(predicted_class, score)
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
-            label = label.encode('utf-8')
-            print(label, top, left, bottom, right)
-            
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
+                label = '{} {:.2f}'.format(predicted_class, score)
+                draw = ImageDraw.Draw(image)
+                label_size = draw.textsize(label, font)
+                label = label.encode('utf-8')
+                print(label, top, left, bottom, right)
+
+                if top - label_size[1] >= 0:
+                    text_origin = np.array([left, top - label_size[1]])
+                else:
+                    text_origin = np.array([left, top + 1])
+
+                for i in range(thickness):
+                    draw.rectangle([left + i, top + i, right - i, bottom - i], outline=self.colors[c])
+                draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=self.colors[c])
+                draw.text(text_origin, str(label, 'UTF-8'), fill=(0, 0, 0), font=font)
+                del draw
             else:
-                text_origin = np.array([left, top + 1])
+                box = None
+                score = None
+                predicted_class = None
 
-            for i in range(thickness):
-                draw.rectangle([left + i, top + i, right - i, bottom - i], outline=self.colors[c])
-            draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=self.colors[c])
-            draw.text(text_origin, str(label,'UTF-8'), fill=(0, 0, 0), font=font)
-            del draw
+
+
+
+
+
+            # wzy打印list
+            # print(list_all)
+            # list_all.sort(key=lambda x: x[1], reverse=False)
+            # print(list_all)
+            # print(len(list_all))
+            #
+            # # 根据top对预测框进行排序，然后计算每个框的hight，以不超过框的2/3为标准进行行的划分
+            # # [predicted_class, top, left, bottom, right]
+            # # list_line1 = [list_all[0]]
+            # # print(list_line1)
+            # list_lines = [[], [], [], [], [], [], [], [], [], []]
+            # global count
+            # global line
+            # count = 0
+            # line = 0
+            # height = 0
+            # max_top = 0
+            # # list_lines[0].append(list_all[0])
+            # # max_top = list_all[0][2]
+            # for line in range(10):
+            #     print("行数:{}".format(line))
+            #
+            #     for i, item in enumerate(list_all, count):
+            #         if not list_lines[line]:
+            #             list_lines[line].append(list_all[count])
+            #             height = list_all[count][3] - list_all[count][1]
+            #             count = count + 1
+            #             continue
+            #         if count == len(list_all):
+            #             break
+            #         if list_all[count][1] < (list_all[count-1][1] + 2/3 * height):
+            #             list_lines[line].append(list_all[count])
+            #             count = count + 1
+            #         else:
+            #             break
+            #     list_lines[line].sort(key=lambda x: x[2], reverse=False)
+            #     print(list_lines[line])
+            #     if count == len(list_all):
+            #         break
+            #
+            # # wzy根据上面得到的排序进行predicted_class顺序输出
+            # print("识别结果为：")
+            # for i in range(line+1):
+            #     # print("----")
+            #     p_class = np.array(list_lines[i])
+            #     result = p_class[:, 0]
+            #     # 将list转换为字符串
+            #     result = list(map(str, result))
+            #     result = ''.join(result)
+            #     print(result)
+            #     # print(p_class[:, 0])
 
         return image
-    
+
     def get_FPS(self, image, test_interval):
         image_shape = np.array(np.shape(image)[0:2])
         #---------------------------------------------------------#
@@ -263,101 +319,6 @@ class YOLO(object):
         tact_time = (t2 - t1) / test_interval
         return tact_time
 
-    def detect_heatmap(self, image, heatmap_save_path):
-        import cv2
-        import matplotlib
-        matplotlib.use('Agg')
-        import matplotlib.pyplot as plt
-        def sigmoid(x):
-            y = 1.0 / (1.0 + np.exp(-x))
-            return y
-        #---------------------------------------------------#
-        #   获得输入图片的高和宽
-        #---------------------------------------------------#
-        image_shape = np.array(np.shape(image)[0:2])
-        #---------------------------------------------------------#
-        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
-        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
-        #---------------------------------------------------------#
-        image       = cvtColor(image)
-        #---------------------------------------------------------#
-        #   给图像增加灰条，实现不失真的resize
-        #   也可以直接resize进行识别
-        #---------------------------------------------------------#
-        image_data  = resize_image(image, (self.input_shape[1],self.input_shape[0]), self.letterbox_image)
-        #---------------------------------------------------------#
-        #   添加上batch_size维度
-        #---------------------------------------------------------#
-        image_data  = np.expand_dims(np.transpose(preprocess_input(np.array(image_data, dtype='float32')), (2, 0, 1)), 0)
-
-        with torch.no_grad():
-            images = torch.from_numpy(image_data)
-            if self.cuda:
-                images = images.cuda()
-            #---------------------------------------------------------#
-            #   将图像输入网络当中进行预测！
-            #---------------------------------------------------------#
-            outputs = self.net(images)
-            
-        outputs = [output.cpu().numpy() for output in outputs]
-        plt.imshow(image, alpha=1)
-        plt.axis('off')
-        mask    = np.zeros((image.size[1], image.size[0]))
-        for sub_output in outputs:
-            b, c, h, w = np.shape(sub_output)
-            sub_output = np.transpose(sub_output, [0, 2, 3, 1])[0]
-            score      = np.max(sigmoid(sub_output[..., 5:]), -1) * sigmoid(sub_output[..., 4])
-            score      = cv2.resize(score, (image.size[0], image.size[1]))
-            normed_score    = (score * 255).astype('uint8')
-            mask            = np.maximum(mask, normed_score)
-            
-        plt.imshow(mask, alpha=0.5, interpolation='nearest', cmap="jet")
-
-        plt.axis('off')
-        plt.subplots_adjust(top=1, bottom=0, right=1,  left=0, hspace=0, wspace=0)
-        plt.margins(0, 0)
-        plt.savefig(heatmap_save_path, dpi=200)
-        print("Save to the " + heatmap_save_path)
-        plt.cla()
-
-    def convert_to_onnx(self, simplify, model_path):
-        import onnx
-        self.generate(onnx=True)
-
-        im                  = torch.zeros(1, 3, *self.input_shape).to('cpu')  # image size(1, 3, 512, 512) BCHW
-        input_layer_names   = ["images"]
-        output_layer_names  = ["output"]
-        
-        # Export the model
-        print(f'Starting export with onnx {onnx.__version__}.')
-        torch.onnx.export(self.net,
-                        im,
-                        f               = model_path,
-                        verbose         = False,
-                        opset_version   = 12,
-                        training        = torch.onnx.TrainingMode.EVAL,
-                        do_constant_folding = True,
-                        input_names     = input_layer_names,
-                        output_names    = output_layer_names,
-                        dynamic_axes    = None)
-
-        # Checks
-        model_onnx = onnx.load(model_path)  # load onnx model
-        onnx.checker.check_model(model_onnx)  # check onnx model
-
-        # Simplify onnx
-        if simplify:
-            import onnxsim
-            print(f'Simplifying with onnx-simplifier {onnxsim.__version__}.')
-            model_onnx, check = onnxsim.simplify(
-                model_onnx,
-                dynamic_input_shape=False,
-                input_shapes=None)
-            assert check, 'assert check failed'
-            onnx.save(model_onnx, model_path)
-
-        print('Onnx model save as {}'.format(model_path))
-        
     def get_map_txt(self, image_id, image, class_names, map_out_path):
         f = open(os.path.join(map_out_path, "detection-results/"+image_id+".txt"),"w") 
         image_shape = np.array(np.shape(image)[0:2])
